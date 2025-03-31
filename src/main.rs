a59e67b31db59e0a23320e85895d91218f8feb43
fengwyn <krzfrancesc@gmail.com>
2025-03-31 16:09:27 -0400

    [WIP] Some bare skeletons to server/client models. Non-functional.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,42 @@
 mod interface;
 use interface::*;
 
+mod client;
+use client::*;
+
+mod server;
+use server::*;
+
+// Command Line Arguments
+use std::env;
+
 fn main() {
 
+    let args: Vec<String> = env::args().collect();
+    let connection_mode = &args[1];
+
+    println!("Connection mode: {connection_mode}\n");
+
     print_commands();
+    
+    match connection_mode {
+
+        "1" => {
+            client();
+        }
+
+        "2" => {
+            server();
+        }
+        
+        _ => {
+            println!("Bad Choice");
+        }
+    }
+
 
     interface();
+
+
 
 }
--- src/server.rs
+++ src/server.rs
@@ -1,1 +1,42 @@
 // server.rs: Contains modules for handling connections, channel management and administrative commands.
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+
+fn handle_client(mut stream: TcpStream, clients: Arc<Mutex<Vec<TcpStream>>>) {
+    
+    clients.lock().unwrap().push(stream.try_clone().unwrap());
+    
+    let mut buffer = [0; 512];
+
+    loop {
+        let bytes_read = stream.read(&mut buffer).unwrap();
+        if bytes_read == 0 {
+            return;
+        }
+
+        let message = String::from_utf8_lossy(&buffer[..bytes_read]).into_owned();
+        println!("Message received: {}", message);
+
+        let clients_guard = clients.lock().unwrap();
+        for client in clients_guard.iter() {
+            client.write_all(message.as_bytes()).unwrap();
+        }
+    }
+}
+
+pub fn server() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let clients = Arc::new(Mutex::new(Vec::new()));
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        let clients = Arc::clone(&clients);
+
+        thread::spawn(move || {
+            handle_client(stream, clients);
+        });
+    }
+}
