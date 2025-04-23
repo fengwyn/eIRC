28f85f0d8916b6ea4ab5fdf897d7aac000437f4c
fengwyn <krzfrancesc@gmail.com>
2025-04-23 09:35:11 -0400

    [WIP] Tracker Object <Parent> with <Child> Objects ServerTracker and ChatServer classes. --- TODO: Implement Tracker working with Client/Server models, will require basic command functionalities in Client

--- src/server/server.py
+++ src/server/server.py
@@ -1,4 +1,4 @@
-# TODO: Encapsulate within Class, allow users create chatrooms and send chatroom invitations
+# TODO: Allow users create chatrooms and send chatroom invitations
 
 # server: Contains modules for handling connections, channel management and administrative commands.
 # NOTE: https://docs.python.org/3.11/howto/sockets.html
@@ -12,7 +12,7 @@ import time
 
 
 # Global Logging Object
-logging.basicConfig(filename="../log/server.log", format='%(asctime)s %(message)s', filemode='a')
+logging.basicConfig(filename="../../log/server.log", format='%(asctime)s %(message)s', filemode='a')
 logger = logging.getLogger()
 
 
@@ -122,13 +122,16 @@ class Server(threading.Thread):
             self.server.shutdown()
             self.server.server_close()
 
+
+# Parameters:  ---hostname <address : Str> --port <port : int> --maxconns <max connections : int> --messagelength <message length: int>
+# Running:      $ python3.13 server.py --hostname localhost --port 8888 --maxconns 32 --messagelength 64
 if __name__ == "__main__":
 
     # Parse command line arguments
     parser = argparse.ArgumentParser(prog="server.py", description="Listens to packets from RF Server nodes.")
     parser.add_argument('-n', '--hostname', type=str, default='localhost', help="Hostname for the Server.")
     parser.add_argument('-p', '--port', type=int, default=8888, help="Port number for Server.")
-    parser.add_argument('-m', '--maxconns', type=int, default=5, help="Maximum Server connections from clients.")
+    parser.add_argument('-m', '--maxconns', type=int, default=32, help="Maximum Server connections from clients.")
     parser.add_argument('-l', '--messagelength', type=int, default=128, help="Message length")
 
     args = parser.parse_args()
